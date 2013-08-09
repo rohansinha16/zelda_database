4e41b638c5693004d2cb791dcdd8ba3902bb1e7b
Rohan Sinha <rohan.sinha16@gmail.com>
2013-08-09 09:38:37 -0500

    got sign in to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-	has_many :votes	
+	has_many :votes, dependent: :destroy
 	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130809083404) do
+ActiveRecord::Schema.define(version: 20130809143015) do
 
   create_table "games", force: true do |t|
     t.string   "name"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20130809083404) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
