6f4e532b942b3262295e0c47b8b857e04b82939c
Rohan Sinha <rohan.sinha16@gmail.com>
2013-08-09 10:15:08 -0500

    Formated votes and set up some validations.

--- app/models/temple.rb
+++ app/models/temple.rb
@@ -1,4 +1,7 @@
 class Temple < ActiveRecord::Base
 	belongs_to :game
 	has_many :items
+
+	validates :name, :uniqueness => true
+	validates :name, :picture_url, :description, :half_boss, :boss, :order, :game_id, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :votes, dependent: :destroy
 	has_secure_password
+
+	validates :username, :uniqueness => true
+	validates :username, :password, :presence => true
 end
